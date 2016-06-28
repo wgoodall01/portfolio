b5539ee805ef99c50a05d43b8c8f267f89a5aea9
William Goodall <wgoodall01@gmail.com>
2016-06-28 14:08:29 -0400

    Travis test 3 - hopefully this works

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-SOURCE_BRANCH="master"
-TARGET_BRANCH="gh-pages"
+# Save some useful information
+TARGET_BRANCH="master"
+REPO="git@github.com:wgoodall01/wgoodall01.github.io.git"
+COMMIT_AUTHOR_EMAIL="wgoodall01@gmail.com"
+SHA=`git rev-parse --verify HEAD`
 
+doCompile () {
+    node $TRAVIS_BUILD_DIR/build.js
+}
+
+#CD to project root
+cd $TRAVIS_BUILD_DIR
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
@@ -12,26 +21,27 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]
     exit 0
 fi
 
-# Save some useful information
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
-SHA=`git rev-parse --verify HEAD`
 
-# Clone the existing gh-pages for this repo into out/
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
+#Clone master from target repo (user pages) into out/
+# Create a new empty branch if master doesn't exist yet (for any reason)
+cd $TRAVIS_BUILD_DIR
 git clone $REPO out
 cd out
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ..
+cd $TRAVIS_BUILD_DIR
 
 # Clean out existing contents
-rm -rf out/**/* || exit 0
+rm -rf $TRAVIS_BUILD_DIR/out/**/* || exit 0
 
-# Run our compile script
-node build.sh
+# Run compile script
+cd $TRAVIS_BUILD_DIR
+doCompile
 
 # Now let's go have some fun with the cloned repo
-cd out
+cd $TRAVIS_BUILD_DIR/out
+
+#Set git configuration
+
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
@@ -46,15 +56,11 @@ fi
 git add .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
+# Get the decrypted deploy key
+
+chmod 600 $TRAVIS_BUILD_DIR/deploy/deploy_key
 eval `ssh-agent -s`
-ssh-add deploy_key
+ssh-add $TRAVIS_BUILD_DIR/deploy/deploy_key
 
 # Now that we're all set up, we can push.
-git push $SSH_REPO $TARGET_BRANCH
+git push $REPO $TARGET_BRANCH
