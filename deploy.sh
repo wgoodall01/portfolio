0ed62bf4f4c4e557592f09d5c8fb98bd2d85a852
William Goodall <wgoodall01@gmail.com>
2016-06-28 01:26:25 -0400

    Get travis working hopefully

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ set -e # Exit with nonzero exit code if anything fails
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
+
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
@@ -27,7 +28,7 @@ cd ..
 rm -rf out/**/* || exit 0
 
 # Run our compile script
-node build.js
+node build.sh
 
 # Now let's go have some fun with the cloned repo
 cd out
@@ -46,7 +47,11 @@ git add .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-echo $DEPLOY_PRIVATE_KEY > deploy_key
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
